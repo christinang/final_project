3717f8afe4df2cb1d5a702f8d0effbff4a0c85c2
cng <username@users.noreply.github.com>
2016-12-08 11:30:19 -0600

    cant delete event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
   validates :event_name, :presence => true, :uniqueness => true
 
-  has_many :charges
+  has_many :charges, :dependent => :restrict_with_error
 end
